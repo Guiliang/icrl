c9ff1b511d97b2efe9774e762103257e8c2d6e97
Guiliang <gla68@sfu.ca>
2022-03-30 16:59:55 +0800

    upload

--- submit_icrl_mujoco_q_vector.sh
+++ submit_icrl_mujoco_q_vector.sh
@@ -9,8 +9,8 @@
 task_name="train-Mojuco-ICRL"
 launch_time=$(date +"%H:%M-%m-%d-%y")
 log_dir="log-${task_name}-${launch_time}.out"
+source /h/galen/miniconda3/bin/activate
+conda activate galen-cr37
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/h/galen/.mujoco/mujoco210/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
-source /h/galen/miniconda3/bin/activate
-conda activate bak-galen-cr37
 python run_me.py icrl -p ICRL-FE2 --group HC-ICRL -er 10 -ep icrl/expert_data/HCWithPos-New -tk 0.01 -cl 20 -bi 10 -ft 2e5 -ni 30 -tei HCWithPos-v0 -eei HCWithPosTest-v0 -clr 0.05 -aclr 0.9 -crc 0.5 -psis -ctkno 2.5 -nt 1 -d cuda -l "$log_dir"
