b242c33692b0964c87186766bf1f208f96fac7c9
Guiliang <gla68@sfu.ca>
2022-03-31 13:15:52 +0800

    upload

--- submit_icrl_mujoco_q_vector.sh
+++ submit_icrl_mujoco_q_vector.sh
@@ -11,8 +11,10 @@ launch_time=$(date +"%H:%M-%m-%d-%y")
 log_dir="log-${task_name}-${launch_time}.out"
 #source /h/galen/miniconda3/bin/activate
 #conda activate galen-cr37
-source /pkgs/anaconda3/bin/activate
-conda activate newgym
+export PATH=/pkgs/anaconda3/bin:$PATH
+export LD_LIBRARY_PATH=/pkgs/mjpro150/bin:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/h/galen/.mujoco/mujoco210/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+source /pkgs/anaconda3/bin/activate
+conda activate newgym
 python run_me.py icrl -p ICRL-FE2 --group HC-ICRL -er 10 -ep icrl/expert_data/HCWithPos-New -tk 0.01 -cl 20 -bi 10 -ft 2e5 -ni 30 -tei HCWithPos-v0 -eei HCWithPosTest-v0 -clr 0.05 -aclr 0.9 -crc 0.5 -psis -ctkno 2.5 -nt 1 -l "$log_dir"
